3aca556175daefedd51b482028ea3c7a79fb235b
Karelys Amador <karelysamador95@gmail.com>
2023-01-28 13:43:18 +0100

    agregar modulo 2 y 3 completos

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,13 +1,18 @@
 import React from "react";
+
 const Heading = () => {
   return (
-    <header className="App-header">
-      <p>CYF Hotel</p>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Hotel_K%C3%A4mp_by_Night_in_Winter_-_panoramio.jpg"
-        alt
-      />
+    <header className="App-header my-flex-row">
+      <span className="my-col-6 my-pa-1 ">CYF Hotel</span>
+      <span className="my-col-6 my-flex-row my-jc-end my-pr-1 my-pt-small">
+        <img
+          className="App-header__image"
+          src="https://cdn-icons-png.flaticon.com/512/4052/4052467.png"
+          alt="hotel"
+        />
+      </span>
     </header>
   );
 };
+
 export default Heading;
